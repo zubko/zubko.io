311565414c340c6dbe5bdade0c67887051312664
Alexander Zubko <alexander.zubko@gmail.com>
2019-07-23 13:59:20 +0300

    ✨ Add more contacts

--- src/components/Link.js
+++ src/components/Link.js
@@ -10,9 +10,7 @@ import { Link as GatsbyLink } from 'gatsby';
 import { Styles } from '../Theme';
 
 export default ({ useAnchor, children, to, ...otherProps }: Object) => {
-  const internal = /^\/(?!\/)/.test(to);
-  // TODO: can't do `css={[Styles.link, css]}` or `css={css || Styles.link}`
-  // because the both classes will end up assigned to the tag in the random order
+  const internal = to === '#' || to.indexOf('/') === 0;
   if (!useAnchor && internal) {
     return (
       <GatsbyLink to={to} css={Styles.link} {...otherProps}>
@@ -20,8 +18,13 @@ export default ({ useAnchor, children, to, ...otherProps }: Object) => {
       </GatsbyLink>
     );
   }
+  const sameTab = to.indexOf('mailto:') === 0;
   return (
-    <a href={to} css={Styles.link} target="_blank" {...otherProps}>
+    <a
+      href={to}
+      css={Styles.link}
+      {...(!sameTab ? { target: '_blank' } : null)}
+      {...otherProps}>
       {children}
     </a>
   );
--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,15 +1,95 @@
 import React from 'react';
 
-import { Layout, SEO, AsciiEmail, Link } from '../components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faCodepen,
+  faLinkedin,
+  faGithubAlt,
+} from '@fortawesome/free-brands-svg-icons';
+import { faAt, faCube } from '@fortawesome/free-solid-svg-icons';
+
+import Layout from '../components/Layout';
+import SEO from '../components/Seo';
+import Link from '../components/Link';
+import ButtonLink from '../components/ButtonLink';
 
 const ContactsPage = () => (
   <Layout>
     <SEO title="Contacts" keywords={[`contacts`]} />
     <h2>Contacts</h2>
-    <p>You can reach me here:</p>
-    <AsciiEmail />
+    <p>You can find me here:</p>
+    <div css={styles.group}>
+      {links.map(l => (
+        <ButtonLink
+          css={styles.button}
+          backgroundColor={l.color}
+          borderColor={l.borderColor}
+          to={l.to}>
+          <FontAwesomeIcon icon={l.icon} css={{ marginRight: '0.4em' }} />
+          {l.title}
+        </ButtonLink>
+      ))}
+    </div>
     <Link to="/">← Home</Link>
   </Layout>
 );
 
 export default ContactsPage;
+
+const styles = {
+  group: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  button: {
+    margin: '0 0.8em 1.5em',
+  },
+};
+
+const links = [
+  {
+    to: 'https://github.com/zubko',
+    title: 'GitHub',
+    icon: faGithubAlt,
+    color: '#333',
+    borderColor: '#000',
+  },
+  {
+    to: 'https://twitter.com/_zubko',
+    title: 'Twitter',
+    icon: faTwitter,
+    color: '#2CA9E1',
+    borderColor: '#2695BC',
+  },
+  {
+    to: 'https://ua.linkedin.com/in/alexanderzubko',
+    title: 'LinkedIn',
+    icon: faLinkedin,
+    color: '#0060A9',
+    borderColor: '#063974',
+  },
+  {
+    to: 'https://codepen.io/zubko',
+    title: 'Codepen',
+    icon: faCodepen,
+    color: '#333',
+    borderColor: '#000',
+  },
+  {
+    to: 'https://codesandbox.io/u/zubko',
+    title: 'CodeSandbox',
+    icon: faCube,
+    color: '#f49916',
+    borderColor: '#dd870b',
+  },
+  {
+    to: 'mailto:alex@zubko.io',
+    title: 'Email',
+    icon: faAt,
+    color: '#2C8127',
+    borderColor: '#1a4d17',
+  },
+];
